3c7d084847d7ed887c9d9271aad79e9e32e54723
henghenghahadada <xing19931122@sina.com>
2017-07-26 09:12:16 +0800

    测试

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function(){
+	if($(window).width()<720){
+			(function (win,doc){
+				function change(){
+					doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/360+'px';
+				}
+				change();
+				win.addEventListener('resize',change,false);
+			})(window,document);
+		}
+	$(window).resize(function(){
+		if($(window).width()<720){
+			(function (win,doc){
+				function change(){
+					doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/360+'px';
+				}
+				change();
+				win.addEventListener('resize',change,false);
+			})(window,document);
+		}
+	})
+	setTimeout(function(){
+		$('.ban_i2').addClass('show');
+	},1000)
+})
